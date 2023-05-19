3f65804009e82e24455b53c83211eb92984b2e0f
kkpk4496 <pavankalyank4496@gmail.com>
2023-05-19 15:22:02 +0000

    ccbp-Trendz

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,75 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+    isLoad: true,
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const options = {
+      method: 'GET',
+      headers: {Authorization: `Bearer ${jwtToken}`},
+    }
+
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const fetchData = await response.json()
+      const updatedData = fetchData.products.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({productsList: updatedData, isLoad: false})
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    const {isLoad} = this.state
+    return (
+      <>
+        {isLoad ? (
+          <Loader
+            className="loader"
+            type="TailSpin"
+            color="#00BFFF"
+            height={50}
+            width={50}
+          />
+        ) : (
+          this.renderProductsList()
+        )}
+      </>
+    )
+  }
+}
+
+export default AllProductsSection
